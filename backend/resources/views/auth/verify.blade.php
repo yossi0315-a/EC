981d142f5da7f361120dc61df84f98ec7a1f678e
yossi0315-a <67111280+yossi0315-a@users.noreply.github.com>
2021-02-21 19:34:22 +0900

    文言の修正

--- backend/resources/views/auth/verify.blade.php
+++ backend/resources/views/auth/verify.blade.php
@@ -5,17 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('メールアドレスの確認') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('あなたのメールアドレスにリンクが送信されました。') }}
                         </div>
                     @endif
 
                     {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('メールが届かなかった場合') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
                         <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
